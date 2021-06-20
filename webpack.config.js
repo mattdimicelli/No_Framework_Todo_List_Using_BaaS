66615223cd0b57a5232cc855cf5f587ac4fbd452
Matt Di Micelli <matthewdimicelli@gmail.com>
2021-06-19 22:21:06 -0400

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,26 @@ module.exports = { //export an object that has the following properties
   optimization: {
     minimize: false,
   },
-  devtool: 'inline-source-map',
+  devtool: 'inline-source-map',  //allows errors to be traced
   entry: './src/index.js',  //the first file in dependency graph
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),  //the path has to be an absolute path.  could also do path.join(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults"}]
+            ]
+          }
+        }
+      }
+    ]
   },
 };
